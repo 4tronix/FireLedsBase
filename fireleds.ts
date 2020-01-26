83a6020c59ee3b4bfbd8a8605e28d1d72f4a93a3
4tronix <Sales@4tronix.co.uk>
2020-01-26 16:36:12 +0000

    Added Rainbow and Shift/Rotate

--- fireleds.ts
+++ fireleds.ts
@@ -19,7 +19,7 @@ namespace fireled
          * Sets the whole Band colour 
          * @param colour Colour to set
          */
-        //% blockId="fire_setBand" block="%band|set 10 band to %colour" 
+        //% blockId="fire_setBand" block="%band|set 11 band to %colour" 
         //% weight=50
         //% parts="fireled"
         setBand(colour: number)
@@ -48,8 +48,8 @@ namespace fireled
             r = (bright * r) >> 8;
             g = (bright * g) >> 8;
             b = (bright * b) >> 8;
-            this.ledBuffer [pixel*3] = r;
-            this.ledBuffer [pixel*3+1] = g;
+            this.ledBuffer [pixel*3] = g;
+            this.ledBuffer [pixel*3+1] = r; // yes, I know. Right?
             this.ledBuffer [pixel*3+2] = b;
         }
 
@@ -68,22 +68,62 @@ namespace fireled
             }
         }
 
+        /* get rgb colour number for Rainbow */
+        private wheel(pos: number): number
+        {
+            /* Generate rainbow colors across 0-255 positions */
+            if (pos < 85)
+		return fromRGB(255 - pos * 3, pos * 3, 0); // Red -> Green
+            else if (pos < 170)
+            {
+                pos = pos - 85
+                return fromRGB(0, 255 - pos * 3, pos * 3); // Green -> Blue
+            }
+            else
+            {
+                pos = pos - 170
+                return fromRGB(pos * 3, 0, 255 - pos * 3); // Blue -> Red
+            }
+        }
+
         /* Set band to rainbow colours Red to Blue */
         setRainbow()
         {
-            
+            let step = 256 / this.numLeds;
+            for (let i=0; i<this.numLeds; i++)
+            {
+               this.setPixel(i, wheel(i * step));
+            }
         }
 
         /* Shift band right one pixel, blanking first pixel */
         shiftBand()
         {
-            
+            let step=3;
+            for (let i=1; i<this.numLeds; i++)
+            {
+                let j = i*step;
+                this.ledBuffer[j+0] = this.ledBuffer[j+0-step];
+                this.ledBuffer[j+1] = this.ledBuffer[j+1-step];
+                this.ledBuffer[j+2] = this.ledBuffer[j+2-step];
+            }
+            this.ledBuffer[0] = 0;
+            this.ledBuffer[1] = 0;
+            this.ledBuffer[2] = 0;
         }
 
-        /* Rotate band one pixel */
+        /* Rotate band right one pixel, last pixel moves to first */
         rotateBand()
         {
-            
+            let step=3;
+            let last = this.numLeds * step - step;
+            let r = this.ledBuffer[last+0];
+            let g = this.ledBuffer[last+1];
+            let b = this.ledBuffer[last+2];
+            this.shiftBand();
+            this.ledBuffer[0] = r;
+            this.ledBuffer[1] = g;
+            this.ledBuffer[2] = b;
         }
 
         /** 
@@ -99,7 +139,25 @@ namespace fireled
 
     }
 
-    function packRGB(a: number, b: number, c: number): number
+    /* get rgb colour number for Rainbow */
+    function wheel(pos: number): number
+    {
+        /* Generate rainbow colors across 0-255 positions */
+        if (pos < 85)
+            return this.fromRGB(255 - pos * 3, pos * 3, 0); // Red -> Green
+        else if (pos < 170)
+        {
+            pos = pos - 85
+            return this.fromRGB(0, 255 - pos * 3, pos * 3); // Green -> Blue
+        }
+        else
+        {
+            pos = pos - 170
+            return this.fromRGB(pos * 3, 0, 255 - pos * 3); // Blue -> Red
+        }
+    }
+
+    function fromRGB(a: number, b: number, c: number): number
     {
         return ((a & 0xFF) << 16) | ((b & 0xFF) << 8) | (c & 0xFF);
     }
